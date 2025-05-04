6ce9db7d420f57cf71ad181ca0ee07fe6b3bae5e
Lukman Tunde OLADOKUN <lukmanoladokun86@gmail.com>
2025-05-04 22:10:03 +0100

    Added ClickCounter App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import { useEffect, useState } from "react";
-import SecondaryNavbar from "./components/SecondaryNavbar";
-import { data } from "./mockData";
+// import { useEffect, useState } from "react";
+import React from "react"
+import ClickCounter from "./components/ClickCounter"
+// import SecondaryNavbar from "./components/SecondaryNavbar";
+// import { data } from "./mockData";
 
 function App() {
-  const [apiData, setApiData] = useState([]);
-  const [totalItems, setTotalItems] = useState(0);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [viewMode, setViewMode] = useState("list");
-  const [currentPage, setCurrentPage] = useState(0);
-  const [itemsPerPage] = useState(8);
+  // const [apiData, setApiData] = useState([]);
+  // const [totalItems, setTotalItems] = useState(0);
+  // const [searchQuery, setSearchQuery] = useState("");
+  // const [viewMode, setViewMode] = useState("list");
+  // const [currentPage, setCurrentPage] = useState(0);
+  // const [itemsPerPage] = useState(8);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const mockdata = data;
-        setApiData(mockdata);
-        setTotalItems(mockdata.length);
-      } catch (error) {
-        console.error("Error fetching data", error);
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const mockdata = data;
+  //       setApiData(mockdata);
+  //       setTotalItems(mockdata.length);
+  //     } catch (error) {
+  //       console.error("Error fetching data", error);
+  //       console.log(error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <>
-      <h1>Hello World</h1>;
-      <SecondaryNavbar
+
+      < ClickCounter />
+      {/* <h1>Hello World</h1>; */}
+      {/* <SecondaryNavbar
         searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
         viewMode={viewMode}
@@ -36,7 +40,7 @@ function App() {
         currentPage={currentPage}
         setCurrentPage={setCurrentPage}
         itemsPerPage={itemsPerPage}
-      />
+      /> */}
     </>
   );
 }
---
+++ src/components/ClickCounter.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const ClickCounter = () => {
+    const [ count, setCount ] = useState(0)
+    const [ message, setMessage ]= useState("")
+
+    const decrement = () => {
+        if (count > 0 ) {
+            setCount(prev => prev - 1 );
+            setMessage("");
+        } else {
+            setMessage("You've reached the limit!");
+        }
+    };
+
+    const increment = () = {
+        setCount(prev => prev + 1);
+        setMessage("");
+    };
+
+
+    return (
+        <main style={styles.container}>
+         <h1> Click Counter App</h1>
+         <h2> Count: {count} </h2>
+         <section>
+            <button onClick={decrement} style={styles.button}>Decrease</button>
+            <button onClick={increment} style={styles.button}>Increase</button>
+         </section>
+         { message && <p style={styles.message}>{message}</p> }
+        </main>
+    )
+}
+
+const styles = {
+    container: {
+        textAlign: "center",
+        marginTop: "50px",
+        fontFamily: "Arial",
+    },
+    button: {
+        padding: "10px 20px",
+        margin: "10px",
+        fontSize: "16px",
+        cursor: "pointer",
+    },
+    message: {
+        color: "red",
+        fontWeight: "bold",
+    }
+};
+
+export default ClickCounter;
+
